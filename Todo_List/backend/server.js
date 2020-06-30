3cf5a8afaf3b21126e9d7cfc06dfe369f607b104
Mihail Bezev <45494812+mivebe@users.noreply.github.com>
2020-06-30 19:25:55 +0300

    sds

--- Todo_List/backend/server.js
+++ Todo_List/backend/server.js
@@ -1,7 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Todo = mongoose.model('Todo', {
   text: String,
@@ -11,6 +11,7 @@ const Todo = mongoose.model('Todo', {
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    todos:[Todo]
   }
   type Todo{
     id: ID!
@@ -19,27 +20,37 @@ const typeDefs = `
   }
   type Mutation {
     createTodo(text:String!): Todo
+    updateTodo(id:ID!, complete: Boolean!):Boolean
+    deleteTodo(id:ID!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
+    todos: () => Todo.find()
   },
   Mutation: {
     createTodo: async (_, { text }) => {
       const todo = new Todo({ text, complete: false });
       await todo.save();
       return todo;
-    }
+    },
+    updateTodo: async (_, { id, complete }) => {
+      await Todo.findByIdAndUpdate(id, { complete });
+      return true;
+    },
+    deleteTodo: async (_, { id }) => {
+      await Todo.findByIdAndRemove(id);
+      return true;
+    },
   }
 };
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'DB Error:'));
-db.once('open', function () {
-  server.start(() => console.log('Server is running on localhost:4000'));
-});
+
 const server = new GraphQLServer({ typeDefs, resolvers });
-mongoose.connection.once("open", () => {
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'DB Connection Error:'));
+db.once('open', function () {
   server.start(() => console.log('Server is running on localhost:4000'));
 });
