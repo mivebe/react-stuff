53e5c68a4a22e64c1d56a6659e87a09adde47742
mivebe <mivebe@abv.bg>
2020-12-03 20:46:31 +0200

    stripe test added

---
+++ stripe-test/pages/api/checkout/[id].ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NewExpression } from 'typescript';
+
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STIPE_SECRET_KEY, { apiVersion: "2020-08-27" });
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const { id } = req.query;
+    const session = await stripe.checkout.sessions.retrieve(id as string)
+    res.status(200).json({ session })
+}
---
+++ stripe-test/pages/checkout.tsx
@@ -0,0 +1,26 @@
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
+
+export default function Checkout() {
+    const handleClick = async (event) => {
+        //call backend
+        const { sessionId } = await fetch('/api/checkout/session', {
+            method: 'POST',
+            headers: {
+
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ quantity: 1 })
+        }).then(res => res.json());
+        //when customer clicks redirect him
+        const stripe = await stripePromise;
+        const { error } = await stripe.redirectToCheckout({
+            sessionId,
+        });
+    }
+    return (<div>
+        <h1>Checkout</h1>
+        <button role="link" onClick={handleClick} >Checkout</button>
+    </div>)
+}
