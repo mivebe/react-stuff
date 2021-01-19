0f95f3ae064091e48296b4c09941edab55976886
mivebe <mivebe@abv.bg>
2021-01-19 15:58:39 +0200

    asd

--- angela-test-app/src/components/test.js
+++ angela-test-app/src/components/test.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import $ from "jquery"
-import "jquery-awesome-cursor"
 
-export default function test() {
-
-    $('#asd')
-        .awesomeCursor('pencil', {
-            color: 'limegreen',
-            size: 24,
-            outline: 'brown'
-        });
-
+const test = () => {
     return (
-        <div id={"asd"} style={{ backgroundColor: "darkblue", width: 500, height: 500 }} >
+        <div>
+
         </div>
     )
 }
+
+export default test
