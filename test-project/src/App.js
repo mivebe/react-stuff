7f3439d53510322bfa739fc256e24a9dced3fab5
mivebe <mivebe@abv.bg>
2021-01-21 11:21:17 +0200

    added routing and some r3f stuff

--- test-project/src/App.js
+++ test-project/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import "./App.css"
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import UnmountAnimation from "./Unmount Animation/UnmountAnimation"
 import Core from "./ThreeJS/Core"
-import Main from "./Three Fiber/Main"
+import Plane from "./rf3-plane/Plane"
 
 
 const App = () => {
     return (
-        <div>
-            {/* <UnmountAnimation /> */}
-            {/* <Core /> */}
-            <Main />
+        <div className='app' >
+            <BrowserRouter>
+                <Switch>
+                    <Redirect path='/' exact to='/plane' />
+                    <Route path='/unmount'>
+                        <UnmountAnimation />
+                    </Route>
+                    <Route path='/three'>
+                        <Core />
+                    </Route>
+                    <Route path='/plane'>
+                        <Plane />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
