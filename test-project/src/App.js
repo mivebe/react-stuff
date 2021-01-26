6d5245fb3df2b5144084906909daed14f75f2e42
mivebe <mivebe@abv.bg>
2021-01-27 01:19:24 +0200

    moved stuff to another repo

--- test-project/src/App.js
+++ test-project/src/App.js
@@ -4,14 +4,14 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import UnmountAnimation from "./Unmount Animation/UnmountAnimation"
 import Core from "./ThreeJS/Core"
 import Plane from "./rf3-plane/Plane"
-
+import Character from './r3f-character/Character';
 
 const App = () => {
     return (
         <div className='app' >
             <BrowserRouter>
                 <Switch>
-                    <Redirect path='/' exact to='/plane' />
+                    <Redirect path='/' exact to='/character' />
                     <Route path='/unmount'>
                         <UnmountAnimation />
                     </Route>
@@ -20,6 +20,9 @@ const App = () => {
                     </Route>
                     <Route path='/plane'>
                         <Plane />
+                    </Route>
+                    <Route path='/character'>
+                        <Character />
                     </Route>
                 </Switch>
             </BrowserRouter>
