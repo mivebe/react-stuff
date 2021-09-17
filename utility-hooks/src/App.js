4d6fadde8c0a49c1e357cea0096ce66e193e176a
mivebe <mivebe@abv.bg>
2021-09-17 09:05:24 +0300

    urility hooks initial commit

---
+++ utility-hooks/src/App.js
@@ -0,0 +1,26 @@
+import ToggleComponent from "./1-useToggle/ToggleComponent"                                  //eslint-disable-line
+import TimeoutComponent from "./2-useTimeout/TimeoutComponent"                               //eslint-disable-line
+import DebounceComponent from "./3-useDebounce/DebounceComponent"                            //eslint-disable-line
+import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent"                //eslint-disable-line
+import ArrayComponent from "./5-useArray/ArrayComponent"                                     //eslint-disable-line
+import PreviousComponent from "./6-usePrevious/PreviousComponent"                            //eslint-disable-line
+import StateWithHistoryComponent from "./7-useStateWithHistory/StateWithHistoryComponent"    //eslint-disable-line
+import StorageComponent from "./8-useStorage/StorageComponent"                               //eslint-disable-line
+import AsyncComponent from "./9-useAsync/AsyncComponent"                                     //eslint-disable-line
+import FetchComponent from "./10-useFetch/FetchComponent"                                    //eslint-disable-line
+
+function App() {
+
+  // return <ToggleComponent />
+  // return <TimeoutComponent />
+  // return <DebounceComponent />
+  // return <UpdateEffectComponent />
+  // return <ArrayComponent />
+  // return <PreviousComponent />
+  // return <StateWithHistoryComponent />
+  // return <StorageComponent />
+  // return <AsyncComponent />
+  // return <FetchComponent />
+}
+
+export default App;
